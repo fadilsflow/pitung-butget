fde39b5629fb7cde587bf0c88f37624775332bf9
WAHYUAKHMADFADILLAH <wahyufadil1140@gmail.com>
2025-02-06 05:02:11 +0700

    feat: complete category feature with filtering and sorting

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -16,18 +16,13 @@ const Page = async () => {
 
     return (
         <div className="h-full bg-background">
-            <div className="flex flex-col border-b bg-card">
-                <div className="container flex flex-wrap items-center justify-between gap-6 px-10 py-5">
+            <div className="container flex flex-wrap border-b justify-between bg-card px-10">
+                <div className=" flex flex-wrap items-center justify-between gap-6  py-5">
                     <p className="text-3xl font-bold">
                         Welcome back, {firstName}... 😜
                     </p>
                 </div>
-            </div>
-            <div className="container flex flex-wrap gap-4 justify-between py-5 px-10">
-                <div className="flex flex-col gap-2 ">
-                    <h1 className="text-2xl font-bold">Overview</h1>
-                </div>
-                <div className="flex items-center gap-3">
+                <div className="flex items-center gap-4">
                     <CreateTransactionDialog trigger={
                         <Button >New income</Button>}
                         type="income"
@@ -38,6 +33,12 @@ const Page = async () => {
                         type="expense"
                     />
                 </div>
+            </div>
+            <div className="container flex flex-wrap gap-4 justify-between py-5 px-10">
+                <div className="flex flex-col gap-2 ">
+                    <h1 className="text-2xl font-bold">Overview</h1>
+                </div>
+
 
             </div>
         </div>
