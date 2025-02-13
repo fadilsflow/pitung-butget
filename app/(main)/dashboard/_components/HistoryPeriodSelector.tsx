2edba0f9ac825e8e39727123041ee9b3b7eb4660
fadill linus <wahyufadil1140@gmail.com>
2025-02-13 20:09:50 +0700

    makan dulu

--- app/(main)/dashboard/_components/HistoryPeriodSelector.tsx
+++ app/(main)/dashboard/_components/HistoryPeriodSelector.tsx
@@ -1,4 +1,9 @@
+import { GetHistoryPeriodsResponseType } from "@/app/api/history-periods/route";
+import SkeletonWrapper from "@/components/skeleton-wrapper";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Period, Timeframe } from "@/lib/types";
+import { Value } from "@prisma/client/runtime/library";
 import { useQuery } from "@tanstack/react-query";
 
 interface Props {
@@ -13,10 +18,68 @@ function HistoryPeriodSelector({
   timeframe,
   setTimeframe,
 }: Props) {
-  const historyPeriods = useQuery({
+  const historyPeriods = useQuery<GetHistoryPeriodsResponseType>({
     queryKey: ["overview", "history", "periods"],
     queryFn: () => fetch(`/api/history-periods`).then((res) => res.json()),
   });
-  return <div className="font-bold text-2xl">HistoryPeriodSelector</div>;
+  return (
+    <div className="flex flex-wrap items-center gap-4">
+      <SkeletonWrapper isLoading={historyPeriods.isFetching} fulllWidth={false}>
+        <Tabs
+          value={timeframe}
+          onValueChange={(value) => setTimeframe(value as Timeframe)}
+        >
+          <TabsList>
+            <TabsTrigger value="year">Year</TabsTrigger>
+            <TabsTrigger value="month">Month</TabsTrigger>
+          </TabsList>
+        </Tabs>
+      </SkeletonWrapper>
+      <div className="flex flex-wrap gap-2 items-center">
+        <SkeletonWrapper isLoading={historyPeriods.isFetching}>
+          <YearSelector
+            period={period}
+            setPeriod={setPeriod}
+            years={historyPeriods.data || []}
+          />
+        </SkeletonWrapper>
+      </div>
+    </div>
+  );
+  0;
 }
 export default HistoryPeriodSelector;
+
+function YearSelector({
+  period,
+  setPeriod,
+  years,
+}: {
+  period: Period;
+  setPeriod: (period: Period) => void;
+  years: GetHistoryPeriodsResponseType;
+}) {
+  return (
+    <Select
+      value={period.year.toString()}
+      onValueChange={(value) => {
+        setPeriod({
+          month: period.month,
+          year: parseInt(value),
+        });
+      }}
+    >
+      <SelectTrigger className="w-[180px]">
+        <SelectValue/>
+      </SelectTrigger>
+      <SelectContent>
+        {years.map(year => (
+        <SelectItem key={year} value={year.toString()}>
+            {year}
+          </SelectItem>
+        )
+        )}
+      </SelectContent>
+    </Select>
+  );
+}
--- app/api/history-periods/route.ts
+++ app/api/history-periods/route.ts
@@ -1,47 +1,45 @@
-"use server"
-import { createClient } from "@/app/utils/supabase/server"
-import { prisma } from "@/lib/prisma"
-import { redirect } from "next/navigation"
-import { NextRequest, NextResponse } from "next/server"
+"use server";
+import { createClient } from "@/app/utils/supabase/server";
+import { prisma } from "@/lib/prisma";
+import { redirect } from "next/navigation";
+import { NextRequest, NextResponse } from "next/server";
 
- 
+export async function GET(request: NextRequest) {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-export async funtion GET(request: NextRequest){
-    const supabase = await createClient()
-    const { data: { user } } = await supabase.auth.getUser()
-
-    if (!user) {
-        redirect("/sign-in")
-    }
-  const periods = getHistoryPeriods(user.id)
-  return NextResponse.json(periods)
-
+  if (!user) {
+    redirect("/sign-in");
+  }
+  const periods = await getHistoryPeriods(user.id);
+  return NextResponse.json(periods);
 }
 
-export type GetHistoryPeriodsResponseType = Awaited<ReturnType<typeof getHistoryPeriods>>
+export type GetHistoryPeriodsResponseType = Awaited<
+  ReturnType<typeof getHistoryPeriods>
+>;
 
-async function getHistoryPeriods( userId: String){
+async function getHistoryPeriods(userId: String) {
   const result = await prisma.monthHistory.findMany({
     where: {
       UserId: userId,
     },
-    select:{
-      year: true
+    select: {
+      year: true,
     },
-    distinct:["year"],
+    distinct: ["year"],
     orderBy: [
       {
-        year: "asc"
-      }
+        year: "asc",
+      },
     ],
-
-  })
-  const years = result.map(el => el.year)
-  if ( years.length === 0 ) {
-    // Return the current year 
-    return [ new Date().getFullYear()]
+  });
+  const years = result.map((el) => el.year);
+  if (years.length === 0) {
+    // Return the current year
+    return [new Date().getFullYear()];
   }
   return years;
 }
-
-
