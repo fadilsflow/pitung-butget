5040172f6487bccbc8e072434f286733134e61aa
fadill linus <wahyufadil1140@gmail.com>
2025-02-13 17:53:05 +0700

    UI: badge component

--- app/(main)/dashboard/_components/History.tsx
+++ app/(main)/dashboard/_components/History.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { Card, CardHeader } from "@/components/ui/card";
 import { GetFormaterCurrency } from "@/lib/helpers";
 import { Period, Timeframe } from "@/lib/types";
 import { UserSettings } from "@prisma/client";
@@ -18,6 +19,13 @@ function History({ userSettings } : {userSettings: UserSettings}) {
   return (
   <div className="container">
       <h2 className="mt-12 text-3xl font-bold">History</h2>
+      <Card className="col-span-12 mt-2 w-full">
+        <CardHeader className="gap-2">
+          <CardTitle className="grid grid-flow-row justify-between gap-2 md:grid-flow-col">
+            <HistoryPeriodSelector period={period} setPeriod={setPeriod} timeframe={timeframe) setTimeframe={setTimeframe}/>
+          </CardTitle>
+        </CardHeader>
+      </Card>
     </div>
   )
 
